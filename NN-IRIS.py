9fbc18ee2134953ddb43b74d6cf9b770f3779e95
KostasEreksonas <k.ereksonas@gmail.com>
2024-04-04 17:31:48 +0300

    Added model performance evaluation metrics

--- NN-IRIS.py
+++ NN-IRIS.py
@@ -2,20 +2,30 @@
 
 # Import libraries
 import pandas as pd
+import numpy as np
 from sklearn.model_selection import train_test_split
 from tensorflow import keras
 
 def load_data():
+    """
+    Load data for the model
+    """
     columns = pd.read_csv("data/iris names.txt", names=['names'])
     df = pd.read_csv("data/iris2.data", sep="\t", names=columns["names"])
     return df
 
 def prepare_data(df):
+    """
+    Prepare data for the model
+    """
     X = df.drop("class", axis=1)
     y = pd.get_dummies(df["class"])
     return X, y
 
 def split_data(df):
+    """
+    Split data into training, validation and testing subsets
+    """
     X, y = prepare_data(df)
     # Split the data into training and testing sets
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.15)
@@ -23,25 +33,41 @@ def split_data(df):
     X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.15)
     return X_train, X_val, X_test, y_train, y_val, y_test
 
-def evaluate(df):
-    X_train, X_val, X_test, y_train, y_val, y_test = split_data(df)
+def create_model(df):
+    """
+    Create a Sequential model
+    """
     model = keras.Sequential()
     model.add(keras.layers.Dense(16, activation='relu', input_shape=(4,)))
     model.add(keras.layers.Dense(3, activation='softmax'))
-    #model.summary()
-    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy']) # Compile the model
-    # Train the model!
-    model.fit(X_train, y_train,
-            batch_size=12,
-            epochs=200,
-            validation_data=(X_val, y_val))
-    # Evaluate the model on the test data
-    _, accuracy = model.evaluate(X_test, y_test)
+    return model
+
+def evaluate(model, X_test, y_test):
+    """
+    Evaluate the model on the test data
+    """
+    _, FN, FP, TN, TP, accuracy, precision, recall = model.evaluate(X_test, y_test)
+    mcc = (TP * TN - FP * FN) / np.sqrt((TP + FP) * (TP + FN) * (TN + FP) * (TN + FN))
     print(f"Test accuracy: {accuracy*100:.2f}%")
+    print(f"Test recall: {recall*100:.2f}%")
+    print(f"Test precision: {precision*100:.2f}%")
+    print(f"TP: {TP}")
+    print(f"TN: {TN}")
+    print(f"FP: {FP}")
+    print(f"FN: {FN}")
+    print(f"Test Matthew coefficient: {mcc * 100:.2f}%")
 
 def main():
     df = load_data()
-    evaluate(df)
+    X_train, X_val, X_test, y_train, y_val, y_test = split_data(df)
+    model = create_model(df)
+    model.summary()
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy', 'recall', 'precision', 'TruePositives', 'TrueNegatives', 'FalsePositives', 'FalseNegatives']) # Compile the model
+    model.fit(X_train, y_train,
+            batch_size=12,
+            epochs=200,
+            validation_data=(X_val, y_val)) # Train the model
+    evaluate(model, X_test, y_test)
 
 if __name__ == "__main__":
     main()
