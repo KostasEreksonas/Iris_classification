cbddfbe722f04757b66091b9d0df0d76868e9ac0
KostasEreksonas <k.ereksonas@gmail.com>
2024-04-04 12:54:28 +0300

    Recreated NN-IRIS model as a Python script

---
+++ NN-IRIS.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+# Import libraries
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from tensorflow import keras
+
+def load_data():
+    columns = pd.read_csv("data/iris names.txt", names=['names'])
+    df = pd.read_csv("data/iris2.data", sep="\t", names=columns["names"])
+    return df
+
+def prepare_data(df):
+    X = df.drop("class", axis=1)
+    y = pd.get_dummies(df["class"])
+    return X, y
+
+def split_data(df):
+    X, y = prepare_data(df)
+    # Split the data into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.15)
+    # Split the training data into training and validation sets
+    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.15)
+    return X_train, X_val, X_test, y_train, y_val, y_test
+
+def evaluate(df):
+    X_train, X_val, X_test, y_train, y_val, y_test = split_data(df)
+    model = keras.Sequential()
+    model.add(keras.layers.Dense(16, activation='relu', input_shape=(4,)))
+    model.add(keras.layers.Dense(3, activation='softmax'))
+    #model.summary()
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy']) # Compile the model
+    # Train the model!
+    model.fit(X_train, y_train,
+            batch_size=12,
+            epochs=200,
+            validation_data=(X_val, y_val))
+    # Evaluate the model on the test data
+    _, accuracy = model.evaluate(X_test, y_test)
+    print(f"Test accuracy: {accuracy*100:.2f}%")
+
+def main():
+    df = load_data()
+    evaluate(df)
+
+if __name__ == "__main__":
+    main()
